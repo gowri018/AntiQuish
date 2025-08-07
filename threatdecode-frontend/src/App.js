af8639c292b6bd3bbb574ba5904307cb149dceb8
Gowri <gowria668@gmail.com>
2025-08-07 06:47:37 +0530

    threatdecode-frontend\src\

--- threatdecode-frontend/src/App.js
+++ threatdecode-frontend/src/App.js
@@ -8,18 +8,17 @@ import ThreatAnalysis from "./components/ThreatAnalysis";
 import Results from "./components/Results";
 import About from "./components/About";
 
+import "./App.css";
+
 function App() {
-  // State to hold scanned URL and analysis results
   const [scannedUrl, setScannedUrl] = useState("");
   const [analysisResult, setAnalysisResult] = useState(null);
 
-  // Called when a QR code URL is scanned or uploaded
   const handleUrlScanned = (url) => {
     setScannedUrl(url);
-    setAnalysisResult(null); // Reset previous results when new URL scanned
+    setAnalysisResult(null);
   };
 
-  // Called when analysis result is ready
   const handleAnalysisComplete = (result) => {
     setAnalysisResult(result);
   };
@@ -44,17 +43,13 @@ function App() {
                 </>
               }
             />
-
             <Route
               path="/results"
               element={
                 <Results scannedUrl={scannedUrl} analysisResult={analysisResult} />
               }
             />
-
             <Route path="/about" element={<About />} />
-
-            {/* Fallback route - redirect to home */}
             <Route path="*" element={<QRScanner onUrlScanned={handleUrlScanned} />} />
           </Routes>
         </main>
@@ -64,4 +59,3 @@ function App() {
 }
 
 export default App;
-
